0eb9896e85488c10d7ecc836a9b2bf29faa38337
Sarvari Khatoon <sarvarikhatoon44@gmail.com>
2026-05-05 22:36:18 +0530

    Add random pays bill functionality

---
+++ Utilities/random-pays-bill.py
@@ -0,0 +1,10 @@
+names_string = input("Enter everybodies name separated by a comma and space: ")
+names = names_string.split(", ")
+
+import random
+
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today!")
